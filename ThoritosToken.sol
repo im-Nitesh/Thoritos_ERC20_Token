7de37f7e5d2e4a36292c2e80a316fbffa5d2e4e9
Nitesh Mehra <96172688+im-Nitesh@users.noreply.github.com>
2024-08-23 13:01:31 +0530

    Add files via upload

---
+++ ThoritosToken.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ThoritosToken is ERC20, Ownable {
+    uint8 private _decimals = 8;
+    uint256 private _totalSupply = 100000 * (10 ** uint256(_decimals));
+
+    mapping(address => uint256) private _lockedUntil;
+
+    constructor(address initialOwner) ERC20("Thoritos", "THOR") Ownable(msg.sender) {
+        _mint(initialOwner, _totalSupply);
+        transferOwnership(initialOwner);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external onlyOwner {
+        _burn(msg.sender, amount);
+    }
+
+    function lockTokens(address account, uint256 time) public onlyOwner {
+        require(time > block.timestamp, "Lock time must be in the future");
+        _lockedUntil[account] = time;
+    }
+
+    function getLockedUntil(address account) public view returns (uint256) {
+        return _lockedUntil[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(block.timestamp >= _lockedUntil[msg.sender], "Tokens are locked");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(block.timestamp >= _lockedUntil[sender], "Tokens are locked");
+        return super.transferFrom(sender, recipient, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+}
+
